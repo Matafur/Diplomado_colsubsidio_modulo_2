d03af1f6b636932201d8922dd39cb065ee569830
matafur <migueltafurr@gmail.com>
2022-10-26 20:39:51 -0500

    26/10/2022

--- Taller_semana1/pruebashenry.js
+++ Taller_semana1/pruebashenry.js
@@ -1,5 +1,21 @@
-function saludar (nombre){
-    return `Hola ${nombre}!`;
+function area(vocal){
+    
+    if('a' === vocal ){
+        return 'Es una vocal'
+    }else if('e' === vocal){
+        return 'Es una vocal'
+    }else if('i' === vocal){
+        return 'Es una vocal'
+    }else if('o' === vocal){
+        return 'Es una vocal'
+    }else if('u' === vocal){
+        return 'Es una vocal'
+    }else if('e' === String){
+        return 'Dato Incorrecto'
+    }else {
+        return 'Dato Incorrecto'
+    }
 }
-let hola = saludar('miguel');
-console.log (hola);
+
+let cuadro = area(55);
+console.log (cuadro);
